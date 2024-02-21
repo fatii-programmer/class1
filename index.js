1888f9436d5f248bce209ebbaf71adf6e6425e8b
fatii-programmer <160323457+fatii-programmer@users.noreply.github.com>
2024-02-21 12:29:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello World');
+console.log('Thank You Govrnor Sindh Kamran Khan Tessori!');
---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log('Hello World');
+
+console.log('Thank You Govrnor Sindh Kamran Khan Tessori!');
